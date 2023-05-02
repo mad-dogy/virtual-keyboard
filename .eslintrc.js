68402e1723223b7a7d12a038eb2a033f19b76395
mad-dogy <120950389+mad-dogy@users.noreply.github.com>
2023-05-02 16:26:07 +0300

    refactor: refactor with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,5 @@
 module.exports = {
-  'env': {
-    'modules': true,
-    'commonjs': true,
-    'es2022': true,
-    'node': true
-  },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 12
-  },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
-  }
+  extends: [
+    'semistandard'
+  ]
 };
